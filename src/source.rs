8d7bf4cecceea4fb48d73973cb29f3d1d8dce6f1
Josh Rule <joshua.s.rule@gmail.com>
2021-07-15 13:20:51 -0700

    enhancement: add SourceChange::reify_substitution

--- src/source.rs
+++ src/source.rs
@@ -1,4 +1,4 @@
-use crate::{Name, Schema, Ty, Type, TypeContext, TypeSchema, Variable};
+use crate::{Name, Schema, Substitution, Ty, Type, TypeContext, TypeSchema, Variable};
 
 /// A way to generate fresh [`Variable`]s.
 ///
@@ -169,6 +169,56 @@ impl SourceChange {
                 let new_body = self.reify_typeschema(body, ctx);
                 ctx.intern_polytype(t_var, new_body)
             }
+        }
+    }
+    /// Reify a [`Substitution`] for use under a new [`Source`].
+    ///
+    /// # Examples
+    /// ```
+    /// # use polytype::{ptp, tp, with_ctx, Source, Substitution, Type, TypeContext, Variable};
+    /// with_ctx(1024, |ctx| {
+    ///    let mut src = Source::default();
+    ///    let a = src.fresh();
+    ///    let b = src.fresh();
+    ///    let _ = src.fresh();
+    ///    let ta = ctx.intern_tvar(Variable(a));
+    ///    let tb = ctx.intern_tvar(Variable(b));
+    ///    let t1 = ctx.arrow(ta, tb);
+    ///    let t2 = tp!(ctx, @arrow[tp!(ctx, int), tp!(ctx, bool)]);
+    ///    let sub1 = Type::unify(&[(t1, t2)], &ctx).unwrap();
+    ///
+    ///    let mut src2 = Source::default();
+    ///    let _ = src2.fresh();
+    ///    let pt = ptp!(ctx, 0, 1; @arrow[tp!(ctx, 0), tp!(ctx, 1)]);
+    ///    let t = pt.instantiate(&ctx, &mut src2);
+    ///    let last = ctx.intern_tvar(Variable(src2.fresh()));
+    ///    let mut sub2 = Substitution::with_capacity(ctx, 32);
+    ///    sub2.add(Variable(2), tp!(ctx, bool));
+    ///
+    ///    let src_change = src.merge(src2, vec![Variable(0), Variable(1)]);
+    ///    let t_reified = src_change.reify_type(t, &ctx);
+    ///    assert_eq!(t_reified.to_string(), "t1 → t5");
+    ///    src_change.reify_substitution(&mut sub2, &ctx);
+    ///    assert_eq!(
+    ///        t_reified.apply(&sub2).apply(&sub1).to_string(),
+    ///        "bool → bool"
+    ///    );
+    /// })
+    ///```
+    ///
+    /// [`Substitution`]: struct.Substitution.html
+    /// [`Source`]: struct.Source.html
+    pub fn reify_substitution<'ctx, N: Name>(
+        &self,
+        sub: &mut Substitution<'ctx, N>,
+        ctx: &TypeContext<'ctx, N>,
+    ) {
+        for (var, tp) in sub.sub.iter_mut() {
+            let contains_var = self.sacreds.contains(var);
+            if !contains_var {
+                *var = Variable(var.0 + self.delta)
+            }
+            *tp = self.reify_type(tp, ctx);
         }
     }
 }
